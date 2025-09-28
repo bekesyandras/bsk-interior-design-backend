b1390ef41a4860264d470e2c2723256fc20fc5dd
Békésy András <andras.bekesy@gmail.com>
2025-09-28 10:58:29 +0200

    chore: Initial project setup: Maven, Docker, Git config

---
+++ src/main/java/hu/progmasters/bskinteriordesignbackend/BskInteriorDesignBackendApplication.java
@@ -0,0 +1,13 @@
+package hu.progmasters.bskinteriordesignbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BskInteriorDesignBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BskInteriorDesignBackendApplication.class, args);
+    }
+
+}
